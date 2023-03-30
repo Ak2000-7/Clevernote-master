b87421b9b41cb9fc9b66e96cc39de505f8a080db
Anjan Kumar <kranjan0606@gmail.com>
2023-03-31 04:03:12 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const port = process.env.port;
+const port = process.env.port || 6010;
 
 app.use([cors(), 
       express.static(__dirname + '/client/build'),
